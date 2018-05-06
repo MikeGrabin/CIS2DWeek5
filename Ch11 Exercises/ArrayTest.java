332de12451e22c93239163aa0f85ee26361a7526
MikeGrabin <38022816+MikeGrabin@users.noreply.github.com>
2018-05-05 22:50:19 -0700

    Add files via upload

---
+++ Ch11 Exercises/ArrayTest.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+
+        double[] arrayDouble = new double [99];
+        for (int i = 0; i < arrayDouble.length; i += 1) {
+            arrayDouble[i] = 100.0 * Math.random();
+        }
+        double doubleValue = 0.0;
+        for(double randomNum: arrayDouble) {
+            doubleValue += randomNum;
+        }
+        double average = doubleValue/arrayDouble.length;
+        System.out.println("Average value: " + average);
+        Arrays.sort(arrayDouble);
+        System.out.println("Median value: " + arrayDouble[arrayDouble.length/2]);
+        for (int i = 8; i <= arrayDouble.length; i = i + 9) {
+            System.out.println("The 9th value: " + arrayDouble[i]);
+        }
+    }
+}
